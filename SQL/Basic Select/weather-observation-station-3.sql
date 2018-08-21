fe37768ea1f4965ff332d3d2d11f8d0f979618e4
Johan <johand@openmailbox.org>
2018-08-20 20:37:29 -0400

    Basic select

---
+++ SQL/Basic Select/weather-observation-station-3.sql
@@ -0,0 +1,8 @@
+-- MySQL
+-- Query a list of CITY names from STATION with even ID numbers only. You may print
+-- the results in any order, but must exclude duplicates from your answer.
+
+
+SELECT DISTINCT city
+FROM station
+WHERE (id % 2) = 0;
