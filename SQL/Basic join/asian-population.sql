02eaea0fd5f85ddd56e4e426770f53d570ee337f
Johan <johand@openmailbox.org>
2018-08-29 21:47:55 -0400

    Basic join

---
+++ SQL/Basic join/asian-population.sql
@@ -0,0 +1,13 @@
+-- MySQL
+-- Given the CITY and COUNTRY tables, query the sum of the populations of all
+-- cities where the CONTINENT is 'Asia'.
+
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+
+SELECT
+    SUM(city.population)
+FROM city
+JOIN country
+    ON city.countrycode = country.code
+WHERE continent = 'Asia';
